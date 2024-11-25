d756c7b10c4aef663bf5b7bfb0f5944be0334612
Adrià Fenoy <afenoy95@gmail.com>
2024-11-25 11:24:18 +0100

    .

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
 // script.js
 
 // Define the email address
-const user = "fenoy.adria";
+const encoded = "ZmVub3kuYWRyaWE=";
 const domain = "gmail.com";
-const email = user + "@" + domain;
+const user = atob(encoded);
 
 // Add the email address to the DOM
 document.addEventListener("DOMContentLoaded", () => {
   const emailSpan = document.getElementById("email");
   if (emailSpan) {
-    const encoded = "ZmVub3kuYWRyaWE=";
-    const domain = "gmail.com";
-    const user = atob(encoded);
     emailSpan.innerHTML = `<a href="mailto:${user}@${domain}">${user}@${domain}</a>`;
   }
 });
