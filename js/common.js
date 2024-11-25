03f242566558157337731939a0c0a66c0c8c2ac1
Adrià Fenoy <afenoy95@gmail.com>
2024-11-25 16:44:21 +0100

    .

--- js/common.js
+++ js/common.js
@@ -1,5 +1,26 @@
 // script.js
 
+// Load HTML content into an element
+async function loadHTML(id, filePath) {
+  const element = document.getElementById(id);
+  if (element) {
+    const response = await fetch(filePath);
+    if (response.ok) {
+      const content = await response.text();
+      element.innerHTML = content;
+    } else {
+      console.error(`Failed to load ${filePath}: ${response.status}`);
+    }
+  }
+}
+
+// Load the header, navbar, and footer
+document.addEventListener("DOMContentLoaded", () => {
+  loadHTML("header", "../html/header.html");
+  loadHTML("navbar", "../html/nav.html");
+  loadHTML("footer", "../html/footer.html");
+});
+
 // Define the email address
 const encoded = "ZmVub3kuYWRyaWE=";
 const domain = "gmail.com";
